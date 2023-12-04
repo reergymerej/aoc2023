98261eed069632a3fd4a6037d1e2b1e8cbf99ddd
Jeremy Greer <jex.grizzle@gmail.com>
2023-12-04 11:21:07 -0500

    day 1, second star

--- day1/index.js
+++ day1/index.js
@@ -3,13 +3,59 @@
 const fs = require('fs')
 const assert = require('assert')
 
+const digitMap = {
+  'zero': '0',
+  'one': '1',
+  'two': '2',
+  'three': '3',
+  'four': '4',
+  'five': '5',
+  'six': '6',
+  'seven': '7',
+  'eight': '8',
+  'nine': '9',
+  '0': '0',
+  '1': '1',
+  '2': '2',
+  '3': '3',
+  '4': '4',
+  '5': '5',
+  '6': '6',
+  '7': '7',
+  '8': '8',
+  '9': '9',
+}
+
+const digitStrings = Object.keys(digitMap)
+
+const digitStringToDigit = (value) => {
+  return digitMap[value]
+}
+
+const getLeftDigit = (value) => {
+  const pattern = new RegExp(`^.*?(${digitStrings.join('|')})`)
+  const result = pattern.exec(value)
+  return digitStringToDigit(result[1])
+}
+
+const getRightDigit = (value) => {
+  const pattern = new RegExp(`.*(${digitStrings.join('|')}).*$`)
+  const result = pattern.exec(value)
+  return digitStringToDigit(result[1])
+}
+
+
 const getFirstAndLastDigits = (value) => {
-  const parts = value.split(/[^\d]*/g).filter(x => x !== '')
-  return parts[0] + parts[parts.length - 1]
+  return getLeftDigit(value) + getRightDigit(value)
 }
 
 const getLineInt = (value) => parseInt(getFirstAndLastDigits(value))
 
+
+assert(getLeftDigit('asdfsixtwo1nine') === '6')
+assert(getRightDigit('asdftwo1nine') === '9')
+
+assert.equal(getFirstAndLastDigits('two1nine'), '29')
 assert(getFirstAndLastDigits('asdf2asdfasdf3asdfasdf7asdf7') === '27')
 assert(getFirstAndLastDigits('treb7uchet') === '77')
 assert(getLineInt('asdf2asdf3asdf8asdfg') === 28)
